940dc22ba835695dfbc5cc54337cf46c2f377bba
Thomas Oliveira <thomas.oliveira@astro.unistra.fr>
2025-11-14 16:26:58 +0100

    o

--- astronomy.py
+++ astronomy.py
@@ -40,7 +40,7 @@ def get_son_position(time=None): #I lost him, please help me to retrieve him
     }
 
 
-def absolute_to_apparent_magnitude(absolute_magnitude, distance_parsecs):
+def 😎(absolute_magnitude, distance_parsecs):
     """
     Convert absolute magnitude to apparent magnitude.
     
