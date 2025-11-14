f80f9330717cef4bbedbd946a3cb866cd5f3be9d
Thomas Oliveira <thomas.oliveira@astro.unistra.fr>
2025-11-14 16:17:32 +0100

    Add a cautionary print statement regarding change history

--- astronomy.py
+++ astronomy.py
@@ -110,3 +110,5 @@ def magnitude_difference_to_brightness_ratio(mag_diff):
         float: Brightness ratio
     """
     return 100 ** (mag_diff / 5)
+
+print("Please never check the first edit of Arn in the change history")
